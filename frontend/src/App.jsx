4ecd79a3d51ba2b1a405993da66db0aa7614bce1
rohinimi <rohini.mittal.it.2018@miet.ac.in>
2025-04-30 13:50:21 +0530

    updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,14 @@ import Todo from './Todo.jsx';
 import './App.css';
 
 const App = () => {
-  return (
+  return (<div>
+
     <div className="app-container">
       <Todo />
     </div>
+      <div style={{backgroundColor:'black',textAlign:'center',color:'white'}}>© 2025 Rohini Mittal. All rights reserved.</div>
+  </div>
+
   );
 };
 
